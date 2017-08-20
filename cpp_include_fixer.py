f8e8dbf0eb90d4e52abcdbb4c9aba24ce2ee28c7
William W Wold <wilcoze@gmail.com>
2017-08-19 23:13:21 -0700

    added 'get_header_dict'

--- cpp_include_fixer.py
+++ cpp_include_fixer.py
@@ -2,6 +2,13 @@
 
 import os
 
+header_ext = ['h', 'hpp']
+source_ext = ['c', 'cpp', 'h', 'hpp']
+
+def split_ext(path):
+	i = path.rsplit('.', 1)
+	return (i[0], i[1]) if len(i) == 2 else (i[0], '')
+
 def get_contents_of_dir(base):
 	return [ os.path.join(base, i) for i in os.listdir(base) ]
 
@@ -23,6 +30,20 @@ def get_all_files(base):
 def get_all_files_with_extension(base, extensions):
 	return [ path for path in get_all_files(base) if has_extension(path, extensions) ]
 
-files = get_all_files_with_extension('dummy', ['h', 'hpp'])
+def get_header_dict(base):
+	headers = {}
+	for path in get_all_files_with_extension(base, header_ext):
+		name = split_ext(os.path.basename(path))[0]
+		if name in headers:
+			if type(headers[name]) != type([]):
+				headers[name] = [headers[name]]
+			headers[name].append(path)
+		else:
+			headers[name] = path
+	return headers
 
-print(files)
+#files = get_all_files_with_extension('dummy', ['h', 'hpp'])
+
+#print(files)
+
+print(get_header_dict('dummy'))
